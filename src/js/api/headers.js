be284f75db156c0adc8502b4b17ecf9ea8eafc3d
Lyn Resell <61920408+lynar13@users.noreply.github.com>
2024-11-12 17:20:33 +0000

    updates structure

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,6 +1,6 @@
 // src/js/api/headers.js
 
-import { API_KEY } from './constants.js'; // Adjust the import path if necessary
+import { API_KEY } from '/src/js/api/constants.js'; 
 
 export function headers(includeContentType = false, includeAuth = true) {
   const headers = new Headers();
--- src/js/api/instance.js
+++ src/js/api/instance.js
@@ -1,6 +1,8 @@
 // src/js/api/instance.js
 
-import NoroffAPI from './index.js';  // Import the NoroffAPI class
+/* Import the NoroffAPI class */
 
-// Create and export an instance of NoroffAPI (no need to pass the base URL)
+import NoroffAPI from '/src/js/api/index.js';  
+
+/* Create and export an instance of NoroffAPI (no need to pass the base URL) */
 export default new NoroffAPI();
